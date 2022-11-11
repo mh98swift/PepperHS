418fda36d8491d222a6673ad221830caefb82c7b
mharari <mharari@varcode.com>
2022-11-11 13:38:08 +0200

    test title VC

--- PepperHSTests/OperationsTableVCTests.swift
+++ PepperHSTests/OperationsTableVCTests.swift
@@ -24,4 +24,18 @@ class OperationsTableVCTests: XCTestCase {
             let sut = try XCTUnwrap(navigation.topViewController as? FinancialOperationsTableVC)
         }
     
+    func test_viewDidLoad_setTitle() throws {
+        let bundle = Bundle(for: FinancialOperationsTableVC.self)
+        let sb = UIStoryboard(name: "Main", bundle: bundle)
+        
+        let initialVC = sb.instantiateInitialViewController()
+        let navigation = try XCTUnwrap(initialVC as? UINavigationController)
+        //if we disconect the TableView from UINavigationController from the
+        let sut = try XCTUnwrap(navigation.topViewController as? FinancialOperationsTableVC)
+        
+        sut.loadViewIfNeeded()
+        
+        XCTAssert((sut.title != nil), "Financial operations")
+    }
+    
 }
