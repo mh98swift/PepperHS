c0abed6a221a2c43075ca13d4b244492c8cb9614
mharari <mharari@varcode.com>
2022-11-11 13:48:03 +0200

    Extract makeSUT method

--- PepperHSTests/OperationsTableVCTests.swift
+++ PepperHSTests/OperationsTableVCTests.swift
@@ -15,27 +15,24 @@ class OperationsTableVCTests: XCTestCase {
 //    }
     
         func test_canInit() throws {
-            let bundle = Bundle(for: FinancialOperationsTableVC.self)
-            let sb = UIStoryboard(name: "Main", bundle: bundle)
-            
-            let initialVC = sb.instantiateInitialViewController()
-            let navigation = try XCTUnwrap(initialVC as? UINavigationController)
-            //if we disconect the TableView from UINavigationController from the
-            let sut = try XCTUnwrap(navigation.topViewController as? FinancialOperationsTableVC)
+            _ = try makeSUT()
         }
     
     func test_viewDidLoad_setTitle() throws {
+        let sut = try makeSUT()
+        sut.loadViewIfNeeded()
+        XCTAssert((sut.title != nil), "Financial operations")
+    }
+    
+    //sytem under test
+    func makeSUT() throws -> FinancialOperationsTableVC{
         let bundle = Bundle(for: FinancialOperationsTableVC.self)
         let sb = UIStoryboard(name: "Main", bundle: bundle)
         
         let initialVC = sb.instantiateInitialViewController()
         let navigation = try XCTUnwrap(initialVC as? UINavigationController)
         //if we disconect the TableView from UINavigationController from the
-        let sut = try XCTUnwrap(navigation.topViewController as? FinancialOperationsTableVC)
-        
-        sut.loadViewIfNeeded()
-        
-        XCTAssert((sut.title != nil), "Financial operations")
+        return try XCTUnwrap(navigation.topViewController as? FinancialOperationsTableVC)
     }
     
 }
