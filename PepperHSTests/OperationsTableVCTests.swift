09db7b86a56090d5d1e2ad786dd913ed9d90cb10
mharari <mharari@varcode.com>
2022-11-11 12:43:49 +0200

    add Test target

---
+++ PepperHSTests/OperationsTableVCTests.swift
@@ -0,0 +1,15 @@
+//
+//  OperationsTableVCTests.swift
+//  PepperHSTests
+//
+//  Created by VCM1 on 11/11/2022.
+//
+
+import XCTest
+
+class OperationsTableVCTests: XCTestCase {
+
+    func test() {
+        XCTFail("Initial failing test")
+    }
+}
